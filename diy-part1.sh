5dfbcb17a999beed8d21328d848b03d95a9e2e70
neavo <neavo@neavo.me>
2021-08-04 21:34:42 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,5 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git chinadns https://github.com/aa65535/openwrt-chinadns' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
