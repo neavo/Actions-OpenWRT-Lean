da91eaca65080e0508a9fb543240eec99a0b9380
neavo <neavo@neavo.me>
2021-08-06 20:16:01 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,3 @@
 # Add a feed source
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-git clone https://github.com/aa65535/openwrt-chinadns.git package/chinadns
-git clone https://github.com/aa65535/openwrt-dist-luci.git package/openwrt-dist-luci
